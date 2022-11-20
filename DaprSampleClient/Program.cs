6c1d0b29ca11fa399efed130ef98345a8821bbdd
mostmand <mostmand.mh@gmail.com>
2022-11-20 14:30:16 +0330

    Add client sample

---
+++ DaprSampleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Dapr.Client;
+
+var client = DaprClient.CreateInvokeHttpClient("myapp");
+var response = await client.GetAsync("hello");
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
+
+using var daprClient = new DaprClientBuilder()
+    .Build();
+try
+{
+    var result = await daprClient.InvokeMethodAsync<string>(HttpMethod.Get, "myapp", "hello", CancellationToken.None);
+    Console.WriteLine(result);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
